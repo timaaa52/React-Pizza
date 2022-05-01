90271a470290c1e0d09f584df07bf665920a7888
Tima <tima52rus@gmail.com>
2022-05-01 16:17:23 +0300

    added to case ADD_PIZZA_TO_CART logic

--- src/redux/reducers/cartReducer.ts
+++ src/redux/reducers/cartReducer.ts
@@ -1,10 +1,15 @@
 import {actionsType, addPizzaToCartACType, setTotalCountACType, setTotalPriceACType} from "../actions/cartActions";
-import {onAddPizzaHandlerType} from "../../pages/Home";
 
-
+export type itemPizzaType = {
+    id: number
+    name: string
+    price: number
+    type: string
+    size: number
+}
 
 type CartStateType = {
-    items: { [key: string]: Array<onAddPizzaHandlerType> }
+    items: { [key: string]: Array<itemPizzaType> }
     totalPrice: number
     totalCount: number
 }
@@ -31,14 +36,19 @@ export const cartReducer = (state = initialState, action: GeneralType) => {
             }
         }
         case actionsType.ADD_PIZZA_TO_CART: {
+            const newItems = {
+                ...state.items,
+                [action.payload.item.id]:  !state.items[action.payload.item.id]
+                    ? [action.payload.item]
+                    : [...state.items[action.payload.item.id], action.payload.item]
+            }
             return {
                 ...state,
-                items: {
-                    [action.payload.item.id]: [
-                        ...state[action.payload.item.id],
-                        action.payload.item
-                    ]
-                }
+                items: newItems,
+                // заимствуем метод concat и через apply всовываем все свойства обьекта newItem и получаем длину, для корректного отображения на странице
+                totalCount: [].concat.apply([], Object.values(newItems)).length,
+                // все тоже как и више, только с помощью редьюса пробегаемся по массиву и свойтве price берем цену, и сумму цен всех пицц выводим на страницу
+                totalPrice: [].concat.apply([], Object.values(newItems)).reduce((acc, val: itemPizzaType) => acc + val.price, 0),
             }
         }
         default: return state;
