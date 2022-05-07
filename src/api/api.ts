613b0f5020d94cf7f46a2df0859d059b7244b2b4
Tima <tima52rus@gmail.com>
2022-05-07 22:03:00 +0300

    updt

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,13 +2,13 @@ import axios from "axios"
 import { sortByType } from "../redux/reducers/filterReducer"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://localhost:3000'
 })
 
 
 export const pizzasApi = {
     getPizzas(category: number | null, sortBy: sortByType) {
-            return instance.get(`http://localhost:3001/pizzas?${ category !== null ? `category=${category}`: ''}&_sort=${sortBy}&_order=${sortBy === 'name' ? 'asc' : 'desc'}`)
+            return instance.get(`/pizzas?${ category !== null ? `category=${category}`: ''}&_sort=${sortBy}&_order=${sortBy === 'name' ? 'asc' : 'desc'}`)
     },
 }
 
